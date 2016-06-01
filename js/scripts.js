000ebefaa8ebcd3ec7f23574fcdfdfea2c6d17be
Alex Jacks and Bo Zhao <bo@>
2016-06-01 11:08:48 -0700

    JavaScript debugged

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $("#address form").submit(function(event) {
-    var first-nameInput = $("input#first-name").val();
-    var last-nameInput = $("input#last-name").val();
+    var first_nameInput = $("input#first-name").val();
+    var last_nameInput = $("input#last-name").val();
     var streetInput = $("input#street").val();
     var cityInput = $("input#city").val();
-
-    $(".first-name").text(first-nameInput);
-    $(".last-name").text(last-nameInput);
+debugger;
+    $(".first-name").text(first_nameInput);
+    $(".last-name").text(last_nameInput);
     $(".street").text(streetInput);
     $(".city").text(cityInput);
 
